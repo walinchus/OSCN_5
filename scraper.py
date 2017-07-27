e3186e0fac42288cc6744bc528960203a58cf025
Lucia Walinchus <Lucia@professionalnewsservices.com>
2017-07-27 16:40:00 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -149,9 +149,9 @@ def scrape_and_look_for_next_link(url):
                 print '%d * %d = %d' % (x, y, x*y)'''
     for county in counties:
         for CaseEndingNumber in CaseEndingNumbers:
-            #next_url = base_url+'GetCaseInformation.aspx?db='+'%s'+'&number=CF-'+str(year)+'-'+'%s'
-            next_url = base_url+'GetCaseInformation.aspx?db='+county+'&number=CF-'+str(year)+'-'+CaseEndingNumber
-            print next_url #% (county, CaseEndingNumber)
+            print "base_url+'GetCaseInformation.aspx?db='+'%s'+'&number=CF-'+str(year)+'-'+'%s'" % (county, CaseEndingNumber)
+            next_url = scrape_and_look_for_next_link()
+            print next_url
             record = {}
             record['URL'] = next_url
             scrape_and_look_for_next_link(next_url)
